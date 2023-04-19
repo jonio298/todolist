b5d39127b7da973f57ee4d845611cd4a8c5edc40
jonio298 <jonio298@gmail.com>
2023-04-18 20:16:00 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleTaskInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleTaskFormSubmit = (event) => {
+    event.preventDefault();
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
+  const handleTaskDelete = (index) => {
+    setTasks(tasks.filter((task, i) => i !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-list">
+      <h1>To-Do List</h1>
+      <form onSubmit={handleTaskFormSubmit}>
+        <input
+          type="text"
+          value={newTask}
+          onChange={handleTaskInputChange}
+          placeholder="Enter a new task..."
+        />
+        <button className="submit" type="submit">Add Task</button>
+      </form>
+      {tasks.length > 0 ? (
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task}
+              <button
+                className="delete-button"
+                onClick={() => handleTaskDelete(index)}
+              >
+                x
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="empty-task">No tasks, add a task</p>
+      )}
+      {/* Counter for tasks left */}
+      <div className="counter">
+        Tasks Left: {tasks.length}
+      </div>
     </div>
   );
 }
